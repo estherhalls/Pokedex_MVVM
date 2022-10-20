747b974c6a36717cfe8943617a79e14eae74126b
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-10-20 13:45:17 -0600

    populated pokedex view model from search service protocol

--- Pokedex_MVVM/Pokedex_MVVM/Pokemon List/PokedexViewModel.swift
+++ Pokedex_MVVM/Pokedex_MVVM/Pokemon List/PokedexViewModel.swift
@@ -10,9 +10,36 @@ protocol PokedexViewModelDelegate: AnyObject {
     
 }
 class PokedexViewModel {
+    var pokedex: Pokedex?
+    var pokedexResults: [PokemonResults]
+    private let pokedexService: PokedexServiceable
+    weak var delegate: PokedexViewModelDelegate?
     
-    weak var delegate: PokedexViewModelDelegate
-    init(delegate: PokedexViewModelDelegate) {
+    /// give above types a value if they were not assigned there
+    init(delegate: PokedexViewModelDelegate, pokedexService: PokedexServiceable = PokedexService()) {
         self.delegate = delegate
+        self.pokedexService = pokedexService
+        loadPokedexResults()
     }
+    
+    func loadPokedexResults() {
+        pokedexService.fetch(from: .pokedex) { [weak self] result in
+            self?.handle(pokedex: result)
+        }
+    }
+    private func handle(pokedex result: Result<Pokedex, NetworkError>) {
+        DispatchQueue.main.async {
+            switch result {
+            case .success(let pokedex):
+                self.pokedex = pokedex
+                self.pokedexResults.append(contentsOf: pokedex.results)
+                self.delegate?.pokedexResultsLoaded()
+            case . failure(let error):
+                print ("Error fetching data",
+                error.localizedDescription)
+                self.delegate?.encounteredAlert(error)
+            }
+        }
+    }
+    
 }
